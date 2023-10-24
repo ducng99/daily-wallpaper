58ad951a40b3909bfef16b073f2cca869b494f8a
ducng99 <tom@ducng.dev>
2023-10-25 10:51:39 +1300

    3rd commit and it might work???

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 mod wallpaper_response;
 
 use chrono::{Duration, Local};
+use clap::Parser;
 use std::path::Path;
 use wallpaper_response::WallpaperResponse;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, default_value = "1920")]
+    width: i32,
+    #[clap(short, long, default_value = "1080")]
+    height: i32,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let current_time = Local::now();
     let current_date_formatted = current_time.format("%Y-%m-%d");
     let yesterday = current_time - Duration::days(1);
@@ -14,6 +25,15 @@ fn main() {
 
     if today_wallpaper == None {
         remove_wallpaper(yesterday_formatted.to_string());
+        if let Some(wallpaper_url) = get_today_wallpaper(args.width, args.height) {
+            let wallpaper_path = Path::new("/tmp/wallpaper-cache")
+                .join(current_date_formatted.to_string())
+                .with_extension("jpg");
+
+            let _ = reqwest::blocking::get(&wallpaper_url)
+                .unwrap()
+                .copy_to(&mut std::fs::File::create(wallpaper_path).unwrap());
+        }
     }
 }
 
@@ -38,20 +58,22 @@ fn remove_wallpaper(date: String) {
     }
 }
 
-fn get_today_wallpaper(width: i32, height: i32) -> String {
+fn get_today_wallpaper(width: i32, height: i32) -> Option<String> {
     let url = format!("https://bingwallpaper.microsoft.com/api/BWC/getHPImages?screenWidth={}&screenHeight={}&env=live", width, height);
-    let request = reqwest::blocking::get(&url).unwrap_or(None);
+    let request = reqwest::blocking::get(&url);
 
-    if request != None {
+    if let Ok(request) = request {
         let response_text = request.text().unwrap_or_default();
         if !response_text.is_empty() {
-            let wallpaper_response: WallpaperResponse = serde_json::from_str(&response_text).unwrap_or(None);
-
-            if wallpaper_response.is_some() {
-                return wallpaper_response.unwrap().images[0].url;
+            if let Ok(wallpaper_response) =
+                serde_json::from_str::<WallpaperResponse>(&response_text)
+            {
+                if let Some(image_metadata) = wallpaper_response.images.get(0) {
+                    return Some(image_metadata.url.to_owned());
+                }
             }
         }
     }
 
-    String::from("")
+    None
 }
